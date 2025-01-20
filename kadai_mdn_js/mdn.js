f6561445fc6159fded3757d809470557f2a38bc7
sorahikouki8432 <torocoo20130328@hotmail.com>
2025-01-20 18:10:03 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+// 今日の日付を取得する全体のコード
+const date2 = new Date(2025 / 10 / 12);
+const y = date2.getFullYear();
+const m = date2.getMonth() + 1;
+const d = date2.getDate();
+
+console.log(y + '年' + m + '月' + d + '日');
+
